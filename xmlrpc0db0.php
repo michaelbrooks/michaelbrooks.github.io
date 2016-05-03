492faf376b31f9668c3b3c3d4a2d5e35d2ab2ff7
Michael Brooks <michael.brooks11@gmail.com>
2016-05-02 19:08:45 -0700

    change base url

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,12 +2,12 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>http://wordpress.org/</engineLink>
-    <homePageLink>https://students.washington.edu/mjbrooks</homePageLink>
+    <homePageLink>/</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="https://students.washington.edu/mjbrooks/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://students.washington.edu/mjbrooks/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://students.washington.edu/mjbrooks/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="https://students.washington.edu/mjbrooks/xmlrpc.php" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
           </apis>
   </service>
 </rsd>
